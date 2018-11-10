2ef2931b7509a7c913c188cabca9d4a49064bef9
ClementDeltel <clement.deltel@protonmail.ch>
2018-11-09 22:34:52 -0600

    move classes to package models

--- src/algorithms/Generation.java
+++
@@ -1,9 +0,0 @@
-package algorithms;
-
-public abstract class Generation {
-
-	protected abstract void apply();
-	protected abstract void setParameters();
-	protected abstract void getProgress();
-	
-}
--- src/algorithms/SquareDiamond.java
+++
@@ -1,5 +0,0 @@
-package algorithms;
-
-public class SquareDiamond extends Generation{
-
-}
