e1b03ae75f1533d792e20abd5cc9db725aea204b
ClementDeltel <clement.deltel@protonmail.ch>
2018-11-09 22:53:07 -0600

    protected methods

--- src/models/algorithms/Generation.java
+++ src/models/algorithms/Generation.java
@@ -11,9 +11,9 @@ package models.algorithms;
 
 public abstract class Generation {
 	
-	public abstract void apply();
-	public abstract void setParameters();
-	public abstract void getProgress();
+	protected abstract void apply();
+	protected abstract void setParameters();
+	protected abstract void getProgress();
 	
 
 }
