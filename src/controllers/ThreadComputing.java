791715479c376366518f80766be3b7e76c0d0484
ClementDeltel <clement.deltel@protonmail.ch>
2018-11-14 19:53:22 -0600

    add AlgorithmModel object

--- src/controllers/ThreadComputing.java
+++ src/controllers/ThreadComputing.java
@@ -10,10 +10,13 @@
 package controllers;
 
 import models.Map;
+import models.algorithms.AlgorithmModel;
 import models.algorithms.Random;
 import models.algorithms.SquareDiamond;
 
 public class ThreadComputing implements Runnable {
+	
+	public AlgorithmModel algorithm;
 	
 	public void run() {
 		SquareDiamond SD = new SquareDiamond(9);
