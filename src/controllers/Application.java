9e399e64e14efa6991ce28ebfce222aae9363a73
ClementDeltel <clement.deltel@protonmail.ch>
2018-11-14 19:52:44 -0600

    add dao object in application class

--- src/controllers/Application.java
+++ src/controllers/Application.java
@@ -1,6 +1,11 @@
 package controllers;
 
+import models.database.DaoModel;
+
 public class Application {
+	
+	public ThreadComputing thread;
+	public DaoModel dao;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
