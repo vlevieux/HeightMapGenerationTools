3f4a50e70a4881072c2369b5bdd6d3b8abdcaf39
Victor LEVIEUX <vlevieux@hawk.iit.edu>
2018-12-04 23:49:10 -0500

    Remove unused quote

--- src/controllers/FXMLController.java
+++ src/controllers/FXMLController.java
@@ -608,7 +608,7 @@ public class FXMLController {
 	
 	@FXML
     private void showLogs(ActionEvent event) {
-		Path watchPath = Paths.get("LoggerAlgorithm.fileLogName").toAbsolutePath();
+		Path watchPath = Paths.get(LoggerAlgorithm.fileLogName).toAbsolutePath();
 		new WatchFileChanges(watchPath).start();
     }
 		
