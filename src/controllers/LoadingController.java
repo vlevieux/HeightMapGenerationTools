a5149c4dcb7222610cd7e2297a0298220f413c39
Victor LEVIEUX <vlevieux@hawk.iit.edu>
2018-12-08 03:44:44 -0500

    Add loading view

---
+++ src/controllers/LoadingController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class LoadingController {
+
+    @FXML
+    private Text text_loading;
+
+    public void initialize() {
+    	
+    	/*time.scheduleAtFixedRate( 
+    	        new java.util.TimerTask() {
+    	            @Override
+    	            public void run() {
+    	            	setLoading();
+    	            }
+    	        }, 0, 500); */
+    }
+}
--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -1,47 +1,79 @@
 package controllers;
 
+import java.util.logging.Handler;
+import java.util.logging.Logger;
+
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import models.database.DaoModel;
 
 public class Main extends Application {
 	
-	//public final static Logger LOGGER = Logger.getLogger( LoggerAlgorithm.class.getName());
+	public final static Logger LOGGER = Logger.getLogger( LoggerAlgorithm.class.getName());
 	
+	public Stage stage2;
+	LoadingController controller;
 	@Override
 	public void start(Stage stage) {
-		checkTable();
 		setupLogger();
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/licenseView.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/loadingView.fxml"));
 			Parent root = (Parent) loader.load();
+			controller = (LoadingController)loader.getController();
             Scene scene = new Scene(root);
-	        stage.setTitle("Licence Activation");
+	        stage.initStyle(StageStyle.UNDECORATED);
 	        stage.getIcons().add(new Image("/images/firstheightmap.jpg"));
 	        stage.setScene(scene);
 	        stage.setResizable(false);
-	        //stage.setOnHiding( event -> {for(Handler h:LOGGER.getHandlers()){h.close();}} );
 	        stage.show();
+	        
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		Platform.runLater(new Runnable() {
+            @Override public void run() {
+        		loading(stage);
+            }
+        });		
 	}
 	
 	public static void main(String[] args) throws InterruptedException {
 		launch(args);
 	}
 
+	private void loading(Stage stage) {
+		checkTable();
+		
+		try {
+			stage.close();
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/licenseView.fxml"));
+			Parent root = (Parent) loader.load();
+            Scene scene = new Scene(root);
+            stage2 = new Stage();
+            stage2.initStyle(StageStyle.DECORATED);
+	        stage2.setTitle("Licence Activation");
+	        stage2.getIcons().add(new Image("/images/firstheightmap.jpg"));
+	        stage2.setScene(scene);
+	        stage2.setResizable(false);
+	        //stage.setOnHiding( event -> {for(Handler h:LOGGER.getHandlers()){h.close();}} );
+	        stage2.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 	private void setupLogger() {
-		//LOGGER.setUseParentHandlers(false);
-		/*Logger globalLogger = Logger.getLogger("global");
+		LOGGER.setUseParentHandlers(false);
+		Logger globalLogger = Logger.getLogger("global");
 		Handler[] handlers = globalLogger.getHandlers();
 		for(Handler handler : handlers) {
 		    globalLogger.removeHandler(handler);
-		}*/
+		}
 		System.setProperty("java.util.logging.SimpleFormatter.format",
                 "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$-6s %2$s %5$s%6$s%n");
 	}
