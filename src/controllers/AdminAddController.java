c96ce13fb465bc6c3629726e4d3338a042b1f38c
Victor LEVIEUX <vlevieux@hawk.iit.edu>
2018-12-08 02:32:24 -0500

    Modify color font

--- src/controllers/AdminAddController.java
+++ src/controllers/AdminAddController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import models.database.DaoModel;
 
@@ -33,19 +34,24 @@ public class AdminAddController {
     
     @FXML
     void adminAddLicense(ActionEvent event) {
-    	if (admin_add_textfield_license.getText().matches("^([A-Z0-9]{4}-){3}[A-Z0-9]{4}$")) {
-    		int add = DaoModel.insertTableLicenses(admin_add_textfield_license.getText(), licenseType, duration);
-    		if (add == 1) {
-    			admin_text_message.setText("This license already exist in the database.");
-    		}
-    		else {
-    			System.out.println(admin_add_textfield_license.getText()+" "+licenseType+" "+duration);
-    		}
-    	} else if (licenseType==0) {
+    	if (licenseType==0) {
+    		admin_text_message.setFill(Color.web("#c72020"));
     		admin_text_message.setText("You must select a license type.");
     	} else if (duration==0) {
+    		admin_text_message.setFill(Color.web("#c72020"));
     		admin_text_message.setText("You must select a duration type.");
+    	} else if (admin_add_textfield_license.getText().matches("^([A-Z0-9]{4}-){3}[A-Z0-9]{4}$")) {
+	    		int add = DaoModel.insertTableLicenses(admin_add_textfield_license.getText(), licenseType, duration);
+	    		if (add == 1) {
+	    			admin_text_message.setFill(Color.web("#c72020"));
+	    			admin_text_message.setText("This license already exist in the database.");
+	    		}
+	    		else {
+	    			admin_text_message.setFill(Color.web("#b4bc4e"));
+	    			admin_text_message.setText("Added license :" + admin_add_textfield_license.getText()+", type : "+licenseType+", duration : "+duration+" days.");
+	    		}
     	} else {
+    		admin_text_message.setFill(Color.web("#c72020"));
     		admin_text_message.setText("The license you have entered does not match the license format.");
     	}
     }
